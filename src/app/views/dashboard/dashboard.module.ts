c8291c91bdf9f0a88c246e375e3f6545588660b9
wenfeng <wenfeng.yang@mailbox.tu-dresden.de>
2021-04-04 22:50:09 +0200

    initial commit get data from api everyday

---
+++ src/app/views/dashboard/dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import {CommonModule} from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    DashboardRoutingModule,
+    ChartsModule,
+    BsDropdownModule,
+    ButtonsModule.forRoot(),
+    CommonModule,
+    HttpClientModule
+  ],
+  declarations: [ DashboardComponent ]
+})
+export class DashboardModule { }
---
+++ src/app/views/dashboard/post.ts
@@ -0,0 +1,15 @@
+export interface covid {
+  cases: number;
+  death: number;
+  recovered: number;
+  casesPer100k: number;
+}
+
+export interface infektion {
+  data: any;
+}
+
+export interface Data {
+  cases: any;
+  date: any;
+}
